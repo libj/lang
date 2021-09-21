d835d8433e1df2e0960440828e030468aa17a57f
Seva Safris <seva@safris.org>
2021-09-21 23:37:48 +0700

    Add overrides for Throwables.addSuppressed(...)

--- src/main/java/org/libj/lang/Throwables.java
+++ src/main/java/org/libj/lang/Throwables.java
@@ -18,6 +18,7 @@ package org.libj.lang;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.List;
 
 /**
  * Utility functions for operations pertaining to {@link Throwable}.
@@ -46,6 +47,108 @@ public final class Throwables {
       return suppressed;
 
     target.addSuppressed(suppressed);
+    return target;
+  }
+
+  /**
+   * Adds each of the specified {@code suppressed} exceptions to the
+   * {@code target}.
+   *
+   * @param <T> The type parameter of the exception.
+   * @param target The target exception.
+   * @param suppressed The suppressed exceptions.
+   * @return The exception based on the method's logic, described above.
+   * @throws IllegalArgumentException If {@code target} or {@code suppressed} is
+   *           null.
+   */
+  public static <T extends Throwable>T addSuppressed(final T target, final Exception[] suppressed) {
+    Assertions.assertNotNull(target);
+    Assertions.assertNotNull(suppressed);
+    return addSuppressed(target, suppressed, 0, suppressed.length);
+  }
+
+  /**
+   * Adds each of the specified {@code suppressed} exceptions to the
+   * {@code target}.
+   *
+   * @param <T> The type parameter of the exception.
+   * @param target The target exception.
+   * @param suppressed The suppressed exceptions.
+   * @param fromIndex The index (inclusive) of the first element in
+   *          {@code suppressed}. If {@code fromIndex > toIndex}, the exceptions
+   *          in {@code suppressed} will be traversed in reverse order.
+   * @param toIndex The index (exclusive) of the last element in
+   *          {@code suppressed}. If {@code toIndex > fromIndex}, the exceptions
+   *          in {@code suppressed} will be traversed in regular order.
+   * @return The exception based on the method's logic, described above.
+   * @throws IllegalArgumentException If {@code target} is null.
+   * @throws IndexOutOfBoundsException If {@code off} is negative, {@code len}
+   *           is negative, or {@code suppressed.length} is less than
+   *           {@code off + len}.
+   */
+  public static <T extends Throwable>T addSuppressed(final T target, final Exception[] suppressed, final int fromIndex, final int toIndex) {
+    Assertions.assertNotNull(target);
+    if (fromIndex == toIndex)
+      return target;
+
+    if (fromIndex < toIndex)
+      for (int i = fromIndex; i < toIndex; ++i)
+        target.addSuppressed(suppressed[i]);
+    else
+      for (int i = fromIndex; i > toIndex; --i)
+        target.addSuppressed(suppressed[i]);
+
+    return target;
+  }
+
+  /**
+   * Adds each of the specified {@code suppressed} exceptions to the
+   * {@code target}.
+   *
+   * @param <T> The type parameter of the exception.
+   * @param target The target exception.
+   * @param suppressed The suppressed exceptions.
+   * @return The exception based on the method's logic, described above.
+   * @throws IllegalArgumentException If {@code target} or {@code suppressed} is
+   *           null.
+   */
+  public static <T extends Throwable>T addSuppressed(final T target, final List<Exception> suppressed) {
+    Assertions.assertNotNull(target);
+    Assertions.assertNotNull(suppressed);
+    return addSuppressed(target, suppressed, 0, suppressed.size());
+  }
+
+  /**
+   * Adds each of the specified {@code suppressed} exceptions to the
+   * {@code target}.
+   *
+   * @param <T> The type parameter of the exception.
+   * @param target The target exception.
+   * @param suppressed The suppressed exceptions.
+   * @param fromIndex The index (inclusive) of the first element in
+   *          {@code suppressed}. If {@code fromIndex > toIndex}, the exceptions
+   *          in {@code suppressed} will be traversed in reverse order.
+   * @param toIndex The index (exclusive) of the last element in
+   *          {@code suppressed}. If {@code toIndex > fromIndex}, the exceptions
+   *          in {@code suppressed} will be traversed in regular order.
+   * @return The exception based on the method's logic, described above.
+   * @throws IllegalArgumentException If {@code target} is null.
+   * @throws IndexOutOfBoundsException If {@code off} is negative, {@code len}
+   *           is negative, or {@code suppressed.length} is less than
+   *           {@code off + len}.
+   */
+  public static <T extends Throwable>T addSuppressed(final T target, final List<Exception> suppressed, final int fromIndex, final int toIndex) {
+    Assertions.assertNotNull(target);
+    if (fromIndex == toIndex)
+      return target;
+
+    if (fromIndex < toIndex)
+      for (int i = fromIndex; i < toIndex; ++i)
+        target.addSuppressed(suppressed.get(i));
+    else
+      for (int i = fromIndex; i > toIndex; --i)
+        target.addSuppressed(suppressed.get(i));
+
     return target;
   }
 
