f811d69cda25004128b0aed85afd84e6b3e62342
Seva Safris <seva@safris.org>
2023-07-15 14:35:51 +0700

    Syntax

--- src/main/java/org/libj/lang/ObjectUtil.java
+++ src/main/java/org/libj/lang/ObjectUtil.java
@@ -192,7 +192,7 @@ public final class ObjectUtil {
     if (a2.length != length)
       return false;
 
-    for (int i = 0; i < length; i++) // [N]
+    for (int i = 0; i < length; ++i) // [N]
       if (!ObjectUtil.equals(a1[i], a2[i]))
         return false;
 
